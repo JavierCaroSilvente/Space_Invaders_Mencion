a92114152a252a67e15bf3711c09ebb72fe1d316
DESKTOP-C01HDPH\Xavi <xavicsilvente@gmail.com>
2022-02-04 10:20:02 +0100

    Ejercicio4

--- space_invaders_mencion/Assets/Scripts/Enemy.cs
+++ space_invaders_mencion/Assets/Scripts/Enemy.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int speedEnemy;
+    public float speedEnemy;
+    State currentState = State.INIT;
     public enum State
     {
+        INIT,
         RIGHT,
         LEFT
     }
@@ -15,20 +17,26 @@ public class Enemy : MonoBehaviour
 
     void Start()
     {
-        state = State.RIGHT;
+        state = State.INIT;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (state == State.RIGHT)
+        switch (state)
         {
-            transform.Translate(Vector3.right * Time.deltaTime * speedEnemy);
-        }
-
-        if (state == State.LEFT)
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * speedEnemy);
+            case State.INIT:
+                transform.Translate(Vector3.right * Time.deltaTime * speedEnemy);
+                ChangeState(State.INIT);
+                break;
+            case State.RIGHT:
+                transform.Translate(Vector3.right * Time.deltaTime * speedEnemy);
+                ChangeState(State.RIGHT);
+                break;
+            case State.LEFT:
+                transform.Translate(Vector3.left * Time.deltaTime * speedEnemy);
+                ChangeState(State.LEFT);
+                break;
         }
 
         if (transform.position.x <= -6.7f)
@@ -39,6 +47,15 @@ public class Enemy : MonoBehaviour
         if (transform.position.x >= 6.7f)
         {
             state = State.RIGHT;
+        }
+    }
+    public void ChangeState(State state)
+    {
+        if(state != currentState)
+        {
+            speedEnemy += 0.5f;
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.75f);
+            currentState = state;
         }
     }
 }
